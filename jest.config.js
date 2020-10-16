6c8d570307886c2f9ce8edfde90d7c2b8687fd47
Shawn Stewart <s.stewart0220@gmail.com>
2020-10-16 00:50:59 -0500

    Added '@/' absolute pathing

--- jest.config.js
+++ jest.config.js
@@ -72,6 +72,7 @@ module.exports = {
     // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
     moduleNameMapper: {
         '\\.(css|scss)$': '<rootDir>/tests/__mocks__/styleMock.js',
+        '@/(.*)': '<rootDir>/src/$1',
     },
 
     // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
